578056210aaeafdff8d7bc95629e70b6791d86ad
majeedkhori <majeedkhori@gmail.com>
2026-03-19 23:34:57 +0000

    modified create-users.py

--- create-users.py
+++ create-users.py
@@ -46,8 +46,8 @@ def main():
 
         #REMOVE THIS COMMENT AFTER YOU UNDERSTAND WHAT TO DO - these statements are currently "commented out" as talked about in class
         #The first time you run the code...what should you do here?  If uncommented - what will the os.system(cmd) statemetn attempt to do?
-        #print(cmd)
-        #os.system(cmd)
+        print(cmd)
+        os.system(cmd)
 
         #REPLACE THIS COMMENT - what is the point of this print statement?
         print("==> Setting the password for %s..." % (username))
@@ -56,15 +56,15 @@ def main():
 
         #REMOVE THIS COMMENT AFTER YOU UNDERSTAND WHAT TO DO - these statements are currently "commented out" as talked about in class
         #The first time you run the code...what should you do here?  If uncommented - what will the os.system(cmd) statemetn attempt to do?
-        #print(cmd)
-        #os.system(cmd)
+        print(cmd)
+        os.system(cmd)
 
         for group in groups:
             #REPLACE THIS COMMENT with one that answers "What is this IF statement looking for and why? If group !='-' what happens?"
             if group != '-':
                 print("==> Assigning %s to the %s group..." % (username,group))
                 cmd = "/usr/sbin/adduser %s %s" % (username,group)
-                #print(cmd)
+                print(cmd)
                 #os.system(cmd)
 
 if __name__ == '__main__':
